9ee14c23e99be001f4ea9c2cb92fa7e5abae9a21
cuikho210 <tonykms210@gmail.com>
2022-05-20 17:19:30 +0700

    Reset branch

--- src/views/accounts/LogoutView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-<div class="container container-sm">
-    <h2 class="mb-4">Đăng xuất</h2>
-
-    <p>Bạn có chắc muốn đăng xuất không?</p>
-
-    <button class="btn btn-primary">Đăng xuất</button>
-</div>
-</template>
