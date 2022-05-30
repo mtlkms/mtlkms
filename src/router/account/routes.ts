49089bead0ab853ec32582ad187a613251d454cd
cuikho210 <tonykms210@gmail.com>
2022-05-31 02:27:54 +0700

    Update router: Account router

--- src/router/account/routes.ts
+++ src/router/account/routes.ts
@@ -1,13 +1,11 @@
+// Router for /account
+
 import * as express from 'express';
-import accountController from './controller/accountController/accountController';
+import accountController from '../../controller/accountController/accountController';
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.send('Hello World!');
-});
-
-router.post('/account', (req, res, next) => {
+router.post('/', (req, res, next) => {
     accountController.create(req.body)
     .then(result => {
         res.send(String(result));
@@ -17,7 +15,7 @@ router.post('/account', (req, res, next) => {
     });
 });
 
-router.put('/account/:username', (req, res, next) => {
+router.put('/:username', (req, res, next) => {
     accountController.changeName(req.params.username, req.body)
     .then(result => {
         res.send(String(result));
@@ -27,7 +25,7 @@ router.put('/account/:username', (req, res, next) => {
     });
 });
 
-router.put('/account/:username/password', (req, res, next) => {
+router.put('/:username/password', (req, res, next) => {
     accountController.changePassword(req.params.username, req.body)
     .then(result => {
         res.send(String(result));
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import * as express from 'express';
 import connection from './dbconnection';
-import routes from './routes';
+
+import routes from './router/routes';
+import accountRouter from './router/account/routes';
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/', routes);
+app.use('/account', accountRouter);
 
 const PORT: number = 3000;
 const server = app.listen(PORT, () => {
